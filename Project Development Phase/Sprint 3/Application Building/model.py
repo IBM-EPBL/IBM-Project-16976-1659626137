1e8cfec2c6b42f3805eaba6faf1aea138be7ba4a
vaseekaran002 <vasee002@gmail.com>
2022-11-18 09:00:14 +0530

    cloud api connection

--- Project Development Phase/Sprint 3/Application Building/model.py
+++ Project Development Phase/Sprint 3/Application Building/model.py
@@ -1,9 +1,6 @@
 from keras_preprocessing import image
 import numpy as np
 from keras.models import load_model
-import json
-# from ibm_watson import VisualRecognitionV3
-# from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import requests
 
 
@@ -18,18 +15,18 @@ def predict(img_src):
   img = image.load_img(img_src, target_size=(128,128), color_mode='grayscale')
   arr = image.img_to_array(img)
   arr = np.expand_dims(arr, axis=0)
-  # API_KEY = "uK6_2BkDeaVcCaMnVSMABbOrpSde4G6uMNgSkXkA55e9"
-  # token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
-  # API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
-  # mltoken = token_response.json()["access_token"]
-  # header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
-  # payload_scoring = {"input_data": [{"values": arr.tolist()}]}
-  # response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/d45d9ae4-908b-4d66-9427-e0a6a07468e9/predictions?version=2022-11-16', json=payload_scoring,
-  # headers=header)
-  # print(type(response_scoring))
-  # print(response_scoring.json())
-  # print("Scoring response")
-  # res  = response_scoring.json()
-  # idx = res["predictions"][0]["values"][0][1]
-  prediction = np.argmax(cnn_model.predict(arr))
-  return predict_alpha(prediction)
+  API_KEY = "uK6_2BkDeaVcCaMnVSMABbOrpSde4G6uMNgSkXkA55e9"
+  token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+  API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+  mltoken = token_response.json()["access_token"]
+  header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+  payload_scoring = {"input_data": [{"values": arr.tolist()}]}
+  response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/d45d9ae4-908b-4d66-9427-e0a6a07468e9/predictions?version=2022-11-16', json=payload_scoring,
+  headers=header)
+  print(type(response_scoring))
+  print(response_scoring.json())
+  print("Scoring response")
+  res  = response_scoring.json()
+  idx = res["predictions"][0]["values"][0][1]
+  #prediction = np.argmax(cnn_model.predict(arr))
+  return predict_alpha(idx)
